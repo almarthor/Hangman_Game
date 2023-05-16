c7763533204ea8d551d9c898f70328cc2d002a25
almarthor <almar.egils@gmail.com>
2023-05-16 16:16:35 +0000

    Update hangman.dart

---
+++ lib/hangman.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Hangman extends StatefulWidget {
+  const Hangman({super.key});
+
+  @override
+  State<Hangman> createState() {
+    return _HangmanState();
+  }
+}
+
+class _HangmanState extends State<Hangman> {
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
