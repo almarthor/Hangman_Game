61c0e9cb857bae7b9ac9e0b80d23742c46056e94
almarthor <almar.egils@gmail.com>
2023-05-19 15:04:14 +0000

    Læra

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Csrcflutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\almar\\OneDrive\\Desktop\\flutter_projects\\lokaverkefni\\Lokaverkefni_Almar" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Csrcflutter\\flutter"
+  "PROJECT_DIR=C:\\Users\\almar\\OneDrive\\Desktop\\flutter_projects\\lokaverkefni\\Lokaverkefni_Almar"
+  "FLUTTER_ROOT=C:\\Csrcflutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\almar\\OneDrive\\Desktop\\flutter_projects\\lokaverkefni\\Lokaverkefni_Almar\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\almar\\OneDrive\\Desktop\\flutter_projects\\lokaverkefni\\Lokaverkefni_Almar"
+  "FLUTTER_TARGET=C:\\Users\\almar\\OneDrive\\Desktop\\flutter_projects\\lokaverkefni\\Lokaverkefni_Almar\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\almar\\OneDrive\\Desktop\\flutter_projects\\lokaverkefni\\Lokaverkefni_Almar\\.dart_tool\\package_config.json"
+)
